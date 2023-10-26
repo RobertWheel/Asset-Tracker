be18565ae99ce3a9984bb0284101dcaf926cae9a
riwheel <risaac4jc@icloud.com>
2023-10-25 22:43:22 -0700

    created the search index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+   get 'dashboard/index'
   resources :employees
   resources :softwares
   resources :devices
   resources :categories
   resources :manufacturers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'search', :to => 'search#index'
 
   # Defines the root path route ("/")
   root "dashboard#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Employee.create({
+    :first_name => "Employee 5",
+    :last_name => "Hong"
+})
+
+Employee.create({
+    :first_name => "Employee 4",
+    :last_name => "Rodriguez"
+})
+
+Employee.create({
+    :first_name => "Employee 3",
+    :last_name => "noe"
+})
+
+Employee.create({
+    :first_name => "Employee 2",
+    :last_name => "Doe"
+})
+
+Employee.create({
+    :first_name => "Employee 1",
+    :last_name => "Rueda"
+})
+
+
+
+
+Manufacture.create({
+     :name => "Apple",
+     :website => "apple.com"
+})
+Manufacture.create({
+     :name => "Apple",
+     :website => "apple.com"
+})
+Manufacture.create({
+     :name => "Apple",
+     :website => "apple.com"
+})
+Manufacture.create({
+     :name => "Dell",
+     :website => "Dell.com"
+})
+Manufacture.create({
+     :name => "Samsung",
+     :website => "Samsung.com"
+})
+
+
+
+
+
+Device.create({
+    :name => "Smart phone 1"
+    :manufacturer => "Apple"
+})
