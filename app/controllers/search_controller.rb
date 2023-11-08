35c239ee68d39dd33ed69d1250460ca5b66dfd69
riwheel <risaac4jc@icloud.com>
2023-11-08 15:58:25 -0800

    made category and manufacturer sections optional in search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
   def index
   @query = params[:query]
-  @devices = Device.where("name like '%#{@query}%'")
+  @category_id =  params[:category_id]
+  @manufacturer_id = params[:manufacturer_id]
+  if @category_id.present?
+  @devices = Device.where("name like '%#{@query}%'").where(category_id: @category_id)
+    else
+      @devices =Device.where("name like '%#{@query}%'")
+      end
   end
+
 end
