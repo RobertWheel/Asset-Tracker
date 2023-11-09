56a3693adfed0674b5ebd95b569f49ba5afbc17c
riwheel <risaac4jc@icloud.com>
2023-11-08 21:42:15 -0800

    fixed visual problem with the colapsable nav bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,13 @@ class SearchController < ApplicationController
   def index
   @query = params[:query]
   @category_id =  params[:category_id]
+
+
+  @devices =Device.where("name like '%#{@query}%'")
+
   @manufacturer_id = params[:manufacturer_id]
   if @category_id.present?
-  @devices = Device.where("name like '%#{@query}%'").where(category_id: @category_id)
-    else
-      @devices =Device.where("name like '%#{@query}%'")
-      end
+     @devices = Device.where(category_id: @category_id)
   end
-
+ end
 end
