103ade24ea2564bfd300e7e3e2a20c5ee9dea1da
riwheel <risaac4jc@icloud.com>
2023-11-08 23:19:10 -0800

    search results work with respected filters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,8 @@ class SearchController < ApplicationController
   @query = params[:query]
   @category_id =  params[:category_id]
   @manufacturer_id = params[:manufacturer_id]
-  @search = Search.new(@query)
+  @search = Search.new(@query, :category_id => @category_id, :manufacturer_id => @manufacturer_id)
   @devices = @search.results
 
-  if @category_id.present?
-     @devices = Device.where(category_id: @category_id)
-  end
  end
-end
+ end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,11 +1,21 @@
 class Search
 
 
-  def initialize(query)
+  def initialize(query, options = {})
     @query = query
+    @category_id = options[ :category_id]
+    @manufacturer_id = options[:manufacturer_id]
   end
 
   def results
-    Courses.where("name like '%#{query}%'")
+    devices = Device.where("name like '%#{@query}%'")
+   if @category_id.present?
+     devices = devices.where(category_id: @category_id)
+    end 
+    if @manufacturer_id.present?
+      devices = devices.where(manufacturer_id: @manufacturer_id)
+
   end
+  devices
+end
 end
